df6946d981541f78741a174d213fcc68c1e0b493
Athena Serene <aserenehoosang@gmail.com>
2022-11-15 20:11:21 -0800

    minor changes

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const budget = require("./models/budget")
 const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use("/static", express.static("public"))
-app.get("/", (req, res) => {
-    // res.send("Hello")
+app.get("/", (req, res) => res.redirect("/budget"))
+app.get("/budget",(req, res)=>{
     res.render("index.ejs", {budget})
 })
-app.get("/")
+app.get("/budget/:id", (req, res)=> {
+    res.render("show.ejs", {
+        budget: budget[req.params.id],
+        index: req.params.id
+    });
+})
 const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log(`The Server is listening on port ${PORT}...`)
